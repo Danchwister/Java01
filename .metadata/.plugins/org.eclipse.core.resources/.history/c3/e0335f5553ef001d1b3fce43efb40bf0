eb1285948ae5fe7d2d5e44640f43fb029a1572df
Daniel <daniel.valiceli@gmail.com>
2023-05-11 22:18:14 -0300

    sos

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c3/e0335f5553ef001d1b3fce43efb40bf0
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author pl
+ *
+ */
+module curso_java {
+}
---
+++ curso_java/src/curso_java/Hellows.java
@@ -0,0 +1,18 @@
+package curso_java;
+
+public class Hellows {
+
+	public static void main(String[] args) {
+		
+		int y = 32;
+		double x = 10.7489327489327;
+		double z = y + x ;	
+		System.out.println("Olá mundo!");
+		System.out.println("Bom dia!");
+		System.out.println(y);
+		System.out.print(z);
+		
+		
+	}
+
+}
